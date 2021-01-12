0c82a7252a8336912f1209b10d7a04af49c0799d
augustlidfeldt <61912828+augustlidfeldt@users.noreply.github.com>
2021-01-12 17:55:09 +0100

    Update run_example.py

--- run_example.py
+++ run_example.py
@@ -1,17 +1,17 @@
 import grad_CAM_pipeline
 
 # Image directory
-img_dir = '/Users/august/ETSN20/Tunnel_images/'
+img_dir = '/Users/ETSN20/images/day'
 
 # Put custom model and weights in same folder
-model_dir = '/Users/august/ETSN20/Model_6_1.0.h5'
+model_dir = '/Users/ETSN20/Tunnel_model.h5'
 weight_dir = model_dir.split('.h5')[0] + '_weights.h5' # name of weights file should be like model name + _weights.h5
 
 # Use for custom model
 labels = ['Empty', 'Person', 'Dog', 'Bike']
 
 # Custom model
-# grad_cam_pipeline_production.run_pipeline(model_dir=model_dir, img_dir=img_dir, weight_dir=weight_dir, labels = labels)
+grad_CAM_pipeline.run_pipeline(model_dir=model_dir, img_dir=img_dir, weight_dir=weight_dir, labels = labels)
 
 # Tensorflow included model
-grad_CAM_pipeline.run_pipeline(model_dir='ResNet50', img_dir=img_dir)
+# grad_CAM_pipeline.run_pipeline(model_dir='ResNet50', img_dir=img_dir)
