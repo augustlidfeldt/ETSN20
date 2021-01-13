a1afa893185b9e461cc9cc6d115fbffa209eb882
Markus Borg <markus.borg@gmail.com>
2021-01-13 13:59:39 +0100

    Refactor for cross-OS paths

--- grad_CAM_pipeline.py
+++ grad_CAM_pipeline.py
@@ -1,3 +1,27 @@
+# MIT License
+#
+#Copyright (c) 2017 François Chollet
+#Copyright (c) 2020 August Lidfeldt, Simon Åberg, Ludwig Hedlund, Arvid Ekblom
+#Copyright (c) 2021 Markus Borg
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
@@ -7,52 +31,46 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.applications.vgg19 import VGG19
 from tensorflow.keras import backend as K
 import numpy as np
-#import keras
 import cv2
 import os
+import pathlib
+
 tf.compat.v1.disable_eager_execution()
 K.clear_session()
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-def setup_model(model_dir, weight_dir=None, custom_model = False):
+def setup_model(model_path, weight_path=None, custom_model=False):
 
-    if weight_dir != None:
+    if weight_path != None:
         ## Use for our model
       custom_model = True
-      model = load_model(model_dir)
-      model.load_weights(weight_dir)
-    elif model_dir == 'VGG16':
+      model = load_model(model_path)
+      model.load_weights(weight_path)
+      print("### Loaded model and weights for: " + model_path)
+    elif model_path == 'VGG16':
         ## Use for VGG16
         model = VGG16(weights='imagenet')
-    elif model_dir == 'ResNet50':
+    elif model_path == 'ResNet50':
       ## Use for ResNet50
       model = ResNet50(weights='imagenet')
-    elif model_dir == 'VGG19':
+    elif model_path == 'VGG19':
         ## Use for VGG19
         model = VGG19(weights='imagenet')
     else:
-        print('Model not supported')
+        print('### Model not supported')
 
     model.summary()
     return model, custom_model
 
-# Create "GradCAM" folder if it does not exist
-def create_filestructure(img_dir):
-    try:
-        if not os.path.exists(img_dir + 'gradCAMs'):
-            os.makedirs(img_dir + 'gradCAMs')
-    except OSError:
-        print('Error: Creating directory of data')
-    return img_dir + 'gradCAMs'
+def run_pipeline(model_path, weights_path, dataset_path, labels=None, same_size_HD_images=True):
+    print('### Model to assess: ' + model_path)
+    model, custom_model = setup_model(model_path, weights_path)
 
-def run_pipeline(model_dir, img_dir, weight_dir=None, img_dest = None, labels=None, same_size_HD_images = True, output_text = True):
-
-    model, custom_model = setup_model(model_dir, weight_dir)
-
-    if img_dest == None:
-        img_dest = create_filestructure(img_dir)
-
-    os.chdir(img_dir)
+    # navigate to input directory and create an output subdirectory
+    os.chdir(dataset_path)
+    output_dir = 'Grad-CAMs'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     # Pick out last convolution layer of model
     last_layer_name = ''
@@ -60,25 +78,21 @@ def run_pipeline(model_dir, img_dir, weight_dir=None, img_dest = None, labels=No
         last_layer_name = model.layers[len(model.layers)-i].name
 
         if('conv' in last_layer_name):
-                print('Last convolution layer is ' + last_layer_name)
+                print('### Last convolution layer is ' + last_layer_name)
                 break;
 
     last_layer = model.get_layer(last_layer_name)
     no_filters = last_layer.output_shape[3]
-    print("Number of filters at last conv_layer is " + str(last_layer.output_shape[3]))
+    print("### Number of filters at last conv_layer is " + str(last_layer.output_shape[3]))
 
     combined_gradCAMs = 0
     combined_img = 0
-    last_image = 0
-
-    img = 0
-
     # loop over images in folder and output
-    for curr_img in sorted(os.listdir()): # -1 for heatmaps folder
-
+    for curr_file in sorted(os.listdir()): # -1 for heatmaps folder
         # The local path to our target image
-        if '.jpg' in curr_img:
-            img_path = curr_img
+        if '.jpg' in curr_file:
+            img_path = curr_file
+            print('### Found a jpg: ' + curr_file)
         else:
             continue
 
@@ -153,6 +167,7 @@ def run_pipeline(model_dir, img_dir, weight_dir=None, img_dest = None, labels=No
 
         # We use cv2 to load the original image
         img = cv2.imread(img_path)
+        last_image = img
 
         # resize heatmap and images depending on if different or all HD format
         if same_size_HD_images:
@@ -168,13 +183,15 @@ def run_pipeline(model_dir, img_dir, weight_dir=None, img_dest = None, labels=No
         # We apply the heatmap to the original image
         heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
 
-        # Add up heatmaps of all images to one combined heatmap
+        # Combine heatmaps of all images to one combined heatmap
         if(i == 0):
-            combined_gradCAMs = heatmap * 0.6/(len(os.listdir(img_dir))-1)
-            combined_img = img /(len(os.listdir(img_dir))-2)
+            file_count = len(os.listdir('.'))
+            combined_gradCAMs = heatmap * 0.6/(file_count-1)
+            combined_img = img /(file_count-2)
         else:
-            combined_gradCAMs = heatmap * 0.6/(len(os.listdir(img_dir))-1) + combined_gradCAMs
-            combined_img = img/(len(os.listdir(img_dir))-2) + combined_img
+            file_count = len(os.listdir('.'))
+            combined_gradCAMs = heatmap * 0.6/(file_count-1) + combined_gradCAMs
+            combined_img = img/(file_count-2) + combined_img
 
         # 0.4 here is a heatmap intensity factor
         current_image = heatmap * 0.4 + img
@@ -184,11 +201,11 @@ def run_pipeline(model_dir, img_dir, weight_dir=None, img_dest = None, labels=No
 
         # set label depending on if custom model is used or not
         if custom_model:
-            label =  str(labels[preds[0].argmax()]) #only custom model with labels at top
+            label = str(labels[preds[0].argmax()]) #only custom model with labels at top
         else:
             label = decode_predictions(preds, top=1)[0][0][1] #only for VGG16
 
-        #position text over images
+        # position text over images
         if same_size_HD_images:
             position = (100,980) # use for HD images, all same size
         else:
@@ -203,12 +220,16 @@ def run_pipeline(model_dir, img_dir, weight_dir=None, img_dest = None, labels=No
          (255, 255, 255, 255), #font color
          3) #font stroke
 
-        name = img_dest + '/gradCAM' + str(curr_img)
-        cv2.imwrite(name, current_image)
-
+        # Save an individual Grad-CAMs in the output folder
+        os.chdir(output_dir)
+        print('Writing files here: ' + os.getcwd())
+        filename = curr_file.split('.jpg')[0] + '-Grad-CAM.jpg'
+        cv2.imwrite(filename, current_image)
+        os.chdir('..')
 
     superimposed_img = combined_gradCAMs + combined_img
 
-    # Save the image to disk
-    cv2.imwrite(img_dest + '/combined_gradCAMs.jpg', superimposed_img)
-    cv2.imwrite(img_dest + '/combined_images.jpg', combined_img)
+    # Save the combined Grad-CAMs in the output folder
+    os.chdir(output_dir)
+    cv2.imwrite('combined_Grad-CAMs.jpg', superimposed_img)
+    cv2.imwrite('combined_images.jpg', combined_img)
